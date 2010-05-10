3f076881ca8e523858d7b2357b33f81807c72c2c
Gang Feng <g-feng@northwestern.edu>
2010-05-10 23:08:13 +0000

    add a check for REACTOME availablitiy
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GeneAnswers@46780 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getREACTOMEPATHTerms.R
+++ R/getREACTOMEPATHTerms.R
@@ -2,10 +2,15 @@
 function(pathDBIDs) {
 	if (length(pathDBIDs) == 0) return(NULL)
 	require(biomaRt)
-	mart <- useMart("REACTOME")
-	datasets <- listDatasets(mart)
-	pathway<-useDataset("pathway",mart)
-	returnTable <- getBM(attributes=c('pathway_db_id', '_displayname'), filters='pathway_db_id_list', values=pathDBIDs, mart=pathway)
-	pathName <- .matrix2list(matrix(c(returnTable[,'pathway_db_id'], returnTable[,'_displayname']), ncol=dim(returnTable)[2], dimnames=list(c(1:dim(returnTable)[1]), colnames(returnTable))), verbose=FALSE)
-	return(pathName[pathDBIDs])
+	if ("REACTOME" %in% toupper(listMarts()[,'biomart']))  {
+		mart <- useMart("REACTOME")
+		datasets <- listDatasets(mart)
+		pathway<-useDataset("pathway",mart)
+		returnTable <- getBM(attributes=c('pathway_db_id', '_displayname'), filters='pathway_db_id_list', values=pathDBIDs, mart=pathway)
+		pathName <- .matrix2list(matrix(c(returnTable[,'pathway_db_id'], returnTable[,'_displayname']), ncol=dim(returnTable)[2], dimnames=list(c(1:dim(returnTable)[1]), colnames(returnTable))), verbose=FALSE)
+		return(pathName[pathDBIDs])
+	} else {
+		stop('REACTOME service is currently not available! Please try later.') 
+	}
+	
 }
