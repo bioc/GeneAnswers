983efecec00c93c7090c51971305856b24a23e23
Gang Feng <g-feng@northwestern.edu>
2010-09-30 16:30:33 +0000

    add "showAllGenes" in geneAnswersHeatmap function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GeneAnswers@49752 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/geneAnswersHeatmap.R
+++ R/geneAnswersHeatmap.R
@@ -1,5 +1,5 @@
 `geneAnswersHeatmap` <-
-function (x, showCats=c(1:5), catTerm=FALSE, geneSymbol=FALSE, catID=FALSE, nameLength='all', ...) {
+function (x, showCats=c(1:5), catTerm=FALSE, geneSymbol=FALSE, catID=FALSE, nameLength='all', showAllGenes=FALSE, ...) {
 	if (is.null(x@genesInCategory[showCats])) stop('specified categories can not be found in x@genesInCategory!')
 	#if (is.null(x@geneExprProfile)) stop('Gene expression file is NULL!')
 	if (is.numeric(showCats)) {
@@ -12,15 +12,13 @@ function (x, showCats=c(1:5), catTerm=FALSE, geneSymbol=FALSE, catID=FALSE, name
 		} else stop('specified categories can not be recognized!')
 	}
 	newList <- x@genesInCategory[showCats]
+	
+	
 	if (is.null(x@geneExprProfile)) newDataMatrix <- NULL
 	else {
 		newDataMatrix <- matrix(as.numeric(as.matrix(x@geneExprProfile[,2:(dim(x@geneExprProfile)[2])])), ncol=(dim(x@geneExprProfile)[2]-1))
 		colnames(newDataMatrix) <- colnames(x@geneExprProfile)[2:dim(x@geneExprProfile)[2]]
 		rownames(newDataMatrix) <- x@geneExprProfile[,1]
-	}
-	if (geneSymbol) {
-		newList <- lapply(newList, getSymbols, x@annLib, missing='name')
-		if (!is.null(newDataMatrix)) rownames(newDataMatrix) <- getSymbols(rownames(newDataMatrix), x@annLib, missing='name')
 	}
 	if (catTerm) {
 		if (x@categoryType %in% c('GO', 'GO.BP', 'GO.CC', 'GO.MF', 'DOLITE', 'KEGG', 'REACTOME.PATH', 'CABIO.PATH')) {
@@ -28,6 +26,11 @@ function (x, showCats=c(1:5), catTerm=FALSE, geneSymbol=FALSE, catID=FALSE, name
 		}else {
 		 	print('Slot categoryType is not recognized! No mapping ...')
 		}
+	}
+	if (showAllGenes) newList <- c(newList, 'All Genes'=list(x@geneInput[,1])) 
+	if (geneSymbol) {
+		newList <- lapply(newList, getSymbols, x@annLib, missing='name')
+		if (!is.null(newDataMatrix)) rownames(newDataMatrix) <- getSymbols(rownames(newDataMatrix), x@annLib, missing='name')
 	}
 	geneAnnotationHeatmap(newList, dataMatrix = newDataMatrix, ...) 		  
 }
